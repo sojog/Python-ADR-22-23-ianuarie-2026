f22f73e96783faaa5193600edf3a986f95f8abbd
Silviu Ojog <sil.ojog@gmail.com>
2026-01-22 10:46:14 +0200

    Intro to tipuri de date

---
+++ 01.hello.py
@@ -0,0 +1,12 @@
+
+
+# Functia print va afisa in consola o valoare
+## Comentariu multiplu este CTRL + /      (windows)
+##                          CMD + /      (mac)
+print("Hello World")
+
+print('Hello World')
+
+print('Hello World')
+
+
---
+++ 02.tipuri_de_date.py
@@ -0,0 +1,30 @@
+
+print(1 + 1)
+print("1" + '1')
+print("T" + 'R')
+
+### PRIMITIVE
+
+## PYTHON este un limbaj LOOSLY TYPE
+
+## BOOL (boolean)
+x = True # sau "False"
+print(x)
+print(type(x))
+
+
+# INT (integer)
+x = 22
+print(x)
+print(type(x))
+
+
+# FLOAT (floating number)
+x = 22.656
+print(x)
+print(type(x))
+
+# STR (string)
+x = "22.656"
+print(x)
+print(type(x))
