fb8cd209088b09def404bc4358bcb0dc95ed4391
Silviu Ojog <sil.ojog@gmail.com>
2026-01-22 12:16:24 +0200

    Execitiul cu raza

---
+++ 12.math.py
@@ -0,0 +1,14 @@
+# math -> este un modul  (functionalitate extena) buil-in(vine instalat cu Python)
+import math as matematica  
+
+print(matematica.pi)
+
+print(matematica.pow(10, 2))
+print(10 ** 2)
+
+
+print(matematica.sqrt(10000))
+
+
+print(matematica.floor(32.312))
+print(matematica.ceil(32.312))
