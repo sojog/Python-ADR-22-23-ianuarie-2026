985e19fbab1b3d99e83bf1d3d3504f15578eb0c8
Silviu Ojog <sil.ojog@gmail.com>
2026-01-22 12:56:42 +0200

    Bucle

---
+++ 16.while.py
@@ -0,0 +1,12 @@
+
+x = 0
+
+# WHILE - este bucla infinita
+while x < 10:
+    print("Mai mic", x)
+    x +=  1
+
+
+while x < -31210:
+    print("Mai mic", x)
+    x +=  1
---
+++ 17.for.py
@@ -0,0 +1,12 @@
+
+
+# for (int i = 0; i < 10; i ++)
+
+for i in range(5):
+    print(i)
+
+for i in range(3):
+    print("Salut", i)
+
+for _ in range(3):
+    print("Ce faci?")
